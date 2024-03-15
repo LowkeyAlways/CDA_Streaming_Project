2d321561f1f14b2550c48e7e0252955a3bdca77f
AndyLS <50944069+LowkeyAlways@users.noreply.github.com>
2024-03-15 17:26:06 +0100

    Update connexion bdd

--- Controller/log.inc.php
+++ Controller/log.inc.php
@@ -1,34 +1,32 @@
 <?php
-        try{// On se connecte à MySQL
-            error_reporting(E_ALL & ~E_WARNING);
-                $prenom = $_POST['prénom'];
-                $nom = $_POST['nom'];
-                $email = $_POST['mail'];
-                $password = $_POST['psw'];
-                $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-                $bdd = new PDO('mysql:host=localhost;dbname=streaming', 'andy', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',$pdo_options));
-                
-                $sql = "SELECT * FROM accounts WHERE login = '$email' ";
-                $result = $bdd->prepare($sql); 
-                $result->execute();
-                    
-                if(isset($_POST['mail']) && isset($_POST['psw'])){
-                    $data = $result->fetchAll();
-                    if(password_verify($password, $data[0]["password"]))
-                    {
-                        echo "<p class=\"success\">Vous êtes bien connectés !
-                        </p>";
-                        $_SESSION['name'] = $prenom;
-                    }
-                    else
-                    {
-                        echo "<p class=\"error\">Identifiants Invalides !!!
-                        </p>";
-                        }
-                    }
-                    }
-                    catch(Exception $e){
-                        die("Erreur de connexion : ".$e->getMessage());
-                    }
 
-        ?>
+try { // On se connecte à MySQL
+    error_reporting(E_ALL & ~E_WARNING);
+    $email = $_POST['mail'];
+    $password = $_POST['psw'];
+    $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+    $bdd = new PDO('mysql:host=localhost;dbname=streaming', 'andy', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', $pdo_options));
+
+    $sql = "SELECT * FROM users WHERE login = '$email' ";
+    $result = $bdd->prepare($sql);
+    $result->execute();
+
+    if (isset($_POST['mail']) && isset($_POST['psw'])) {
+        $data = $result->fetchAll();
+        if (password_verify($password, $data[0]["password"])) {
+            echo "<p class=\"alert\">Vous êtes bien connectés !
+                        </p>";
+            $_SESSION['name'] = $data[0]["prénom"];
+           // header('Location: index.php');
+
+            echo "<script>";
+            echo "console.log('Contenu de la variable de session name : " . $_SESSION['name'] . "')";
+            echo "</script>";
+        } else {
+            echo "<p class=\"alert\">Identifiants Invalides !!!
+                        </p>";
+        }
+    }
+} catch (Exception $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
--- Controller/traitement.inc.php
+++ Controller/traitement.inc.php
@@ -1,51 +1,45 @@
-<?php 
+<?php
 
-try{
-    if(isset($_POST['submit'])){
+try {
+    if (isset($_POST['submit'])) {
+        error_reporting(E_ALL & ~E_WARNING);
         $nom = $_POST['nom'];
         $prenom = $_POST['prénom'];
         $email = $_POST['mail'];
         $password = $_POST['psw'];
+        $pays = $_POST['pays'];
+
 
         // On se connecte à MySQL
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        $bdd = new PDO('mysql:host=localhost;dbname=streaming', 'andy', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',$pdo_options));
-        
+        $bdd = new PDO('mysql:host=localhost;dbname=streaming', 'andy', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', $pdo_options));
+
         // Verifie si l'utilisateur est déja inscrit
-        $sql = "SELECT * FROM accounts WHERE login = '$email' ";
+        $sql = "SELECT * FROM users WHERE login = '$email' ";
         $result = $bdd->prepare($sql);
         $result->execute();
 
-        if($result->rowCount() > 0){
-            if(password_verify($password, $data['password']))
-            {
-                echo "<p class=\"success\">Vous êtes déja inscrit, 
-                <a href=\"connexion.php\" title=\"pub\">Connectez vous</a>
+        if ($result->rowCount() > 0) {
+            if (password_verify($password, $data['password'])) {
+                echo "<p class=\"alert\">Vous êtes déja inscrit, 
+                <a href=\"../View/Login.php\" class=\"success\" title=\"pub\">Connectez vous</a>
                 </p>";
-                $_SESSION['email'] = $prenom;   
-            }else
-            {
-                
-                echo "<p class=\"error\">Cette adresse mail est déja utilisé !!!</p>";
-                }
-                
-            
-        }
-        else
-        {
+                $_SESSION['email'] = $email;
+            } else {
+
+                echo "<p class=\"alert\">Cette adresse mail est déja utilisé !!!</p>";
+            }
+        } else {
             $password = password_hash($password, PASSWORD_DEFAULT);
-            $sql = "INSERT INTO accounts (nom, prnom, login, password) VALUES ('$nom', '$prenom', '$email', '$password')";
+            $sql = "INSERT INTO users (nom, prénom, login, password, pays) VALUES ('$nom', '$prenom', '$email', '$password', '$pays')";
             $req = $bdd->prepare($sql);
             $req->execute();
-            echo "<p class=\"success\">Inscription effectuée :
-                 <a href=\"connexion.php\" title=\"pub\">Connectez vous</a>
+            echo "<p class=\"alert\">Inscription effectuée :
+                 <a href=\"../View/Login.php\" class=\"success\" title=\"pub\">Connectez vous</a>
                  </p>";
         }
         $result->closeCursor();
-    }}
-    catch(Exception $e){
-        die("Erreur de connexion : ".$e->getMessage());
     }
-
-
-?>
+} catch (Exception $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
